0485a8648287bb2c4979370eddcdbd7218fd6c6a
magcho <mail@magcho.com>
2020-09-29 21:33:32 +0900

    [add] builded files

--- docs/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-X_LANG=${1:-es}
-
-make -e SPHINXOPTS="-D language='$X_LANG' -t lang_$X_LANG" -e LANG="$X_LANG" html
--- docs/update_locale.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-make gettext && sphinx-intl update -p build/es/locale/ -l en
